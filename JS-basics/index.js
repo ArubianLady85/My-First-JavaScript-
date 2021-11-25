5e2ffc9f7981a4f691c6f9d76df6551868d4faa4
ArubianLady85 <88424294+ArubianLady85@users.noreply.github.com>
2021-11-25 15:03:04 +0100

    Add files via upload

---
+++ JS-basics/index.js
@@ -0,0 +1,21 @@
+let name = 'Liesje';
+console.log(name = 'Liesje');
+
+let age = 36;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
+
+Liesje
+typeof name
+'string'
+name = 1
+1
+typeof name
+'number'
+typeof age
+'number'
+age = "36"
+'36'
+typeof age
+'string'
